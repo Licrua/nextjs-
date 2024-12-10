8134a5042dfd193609c0a3f4c0e107ea587e425c
Илья Копендох <ilakopendoh@MacBook-Air-Ila.local>
2024-12-10 17:10:40 +0300

    trial

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -35,6 +35,7 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
 	  <Suspense fallback={<RevenueChartSkeleton />}>
+	  <p>pavel zal</p>
           <RevenueChart />
         {/* <RevenueChart revenue={revenue}  /> */}
         </Suspense>
