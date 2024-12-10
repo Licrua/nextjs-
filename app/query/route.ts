75e7046e3077a9b3a4e7d013a4437dcb7efbca44
Илья Копендох <ilakopendoh@MacBook-Air-Ila.local>
2024-12-10 16:15:43 +0300

    loading logic realized

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,28 @@
-// import { db } from "@vercel/postgres";
+import { db } from "@vercel/postgres";
 
-// const client = await db.connect();
+const client = await db.connect();
 
-// async function listInvoices() {
-// 	const data = await client.sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+async function listInvoices() {
+	const data = await client.sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
 
-// 	return data.rows;
-// }
+	return data.rows;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
-}
+	try {
+	  // Выполнение асинхронной операции
+	  const invoices = await listInvoices();
+  
+	  // Возвращаем успешный ответ с данными
+	  return Response.json(invoices);
+	} catch (error) {
+	  // Обработка ошибок и возвращение ответа с ошибкой
+	  return Response.json({ error: error.message || 'An unexpected error occurred' }, { status: 500 });
+	}
+  }
+  
